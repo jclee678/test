9b610c7adc1462d44bd99984cc7e6d015032adde
jclee678 <jclee678@yahoo.com>
2017-05-14 18:06:37 -0700

    Add files via upload

---
+++ initNode.sh
@@ -0,0 +1,5 @@
+./geth --datadir "~/work/blockchain/ethereum/private/private1" init gensisBlock.json
+./geth --datadir "~/work/blockchain/ethereum/private/private2" init gensisBlock.json
+./geth --datadir "~/work/blockchain/ethereum/private/private3" init gensisBlock.json
+
+
