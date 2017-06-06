274f25165c720addaa5f0c927949ef163f07ef54
jclee678 <jclee678@yahoo.com>
2017-06-06 14:25:25 -0700

    Delete newRunNode1.sh

--- newRunNode1.sh
+++
@@ -1,2 +0,0 @@
-./geth --identity "nodeA" --rpc --rpcport "8008" --rpccorsdomain "*" --datadir "~/work/blockchain/ethereum/private/private1" --port "30303" --ipcpath "/Users/test/Library/Ethereum/geth.ipc" --ipcapi "admin,db,eth,debug,miner,net,shh,txpool,personal,web3" --rpcapi "personal,db,eth,net,web3"  --networkid 987654321 --nat "any" --mine --minerthreads 1 console
-
